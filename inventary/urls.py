8dfd6df54cd5b3c2a9341781d45e1ae89e2b014f
jorge <jdgarcia893>
2020-05-10 19:46:02 -0500

    todas la clases lista de product y group con algunos estilos

--- inventary/urls.py
+++ inventary/urls.py
@@ -1,14 +1,26 @@
 from django.urls import path
-from .views import ProductCreateView, ProductListView, ProductUpdateView,GroupCreateView,GroupListView, GroupUpdateView
+from .views import ProductCreateView, ProductListView, ProductUpdateView,\
+GroupCreateView, GroupListView, GroupUpdateView, ProductDetailView, GroupDetailView, \
+ShoppingCartCreateView
 
 
 app_name = "inventary"
 urlpatterns = [
     path('product/create/', ProductCreateView.as_view(), name="CreateProduct"),
     path('list/', ProductListView.as_view(), name="ListProduct"),
-    path('product/update/<int:pk>/',ProductUpdateView.as_view(), name="UpdateProduct"),
-    path('group/create/', GroupCreateView.as_view(),name="CreateGroup"),
-    path('group/update/<int:pk>/',GroupUpdateView.as_view(),name="UpdateGroup"),
-    path('group/list/',GroupListView.as_view(),name="ListGroup"),
+    path('product/update/<int:pk>/',
+         ProductUpdateView.as_view(), name="UpdateProduct"),
+    path('Product/<int:pk>/', ProductDetailView.as_view(), name="DetailProduct"),
+
+
+    path('group/create/', GroupCreateView.as_view(), name="CreateGroup"),
+    path('group/update/<int:pk>/', GroupUpdateView.as_view(), name="UpdateGroup"),
+    path('group/list/', GroupListView.as_view(), name="ListGroup"),
+    path('group/<int:pk>/', GroupDetailView.as_view(), name="DetailGroup"),
+
+    #urls del shoppingcart
+    path('shoppingcart/create/',ShoppingCartCreateView.as_view(),name="CreateShopping"),
+    path('shoppingcart/create/',ShoppingCartCreateView.as_view(),name="ListShopping"),
+
 
 ]
--- inventary/views.py
+++ inventary/views.py
@@ -1,13 +1,14 @@
-from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 from django.urls import reverse_lazy
 from .models import ProductModel, GroupProductModel, ShoppingCartModel
-from .forms import ProductForm,GroupProductForm
+from .forms import ProductForm, GroupProductForm
 
 
 class ProductCreateView(CreateView):
     model = ProductModel
     form_class = ProductForm
     template_name = "product/CreateProduct.html"
+    success_url = reverse_lazy('inventary:ListProduct')
 
 
 class ProductListView(ListView):
@@ -16,26 +17,61 @@ class ProductListView(ListView):
     template_name = "product/ListProduct.html"
 
 
+class ProductDetailView(DetailView):
+    model = ProductModel
+    context_object_name = "product"
+    template_name = "product/DetailProduct.html"
+
+
 class ProductUpdateView(UpdateView):
     model = ProductModel
     form_class = ProductForm
     template_name = "product/UpdateProduct.html"
 
+    def get_success_url(self):
+        product_id = self.kwargs['pk']
+        return reverse_lazy('inventary:DetailProduct',kwargs={'pk': product_id})
+
 
 class GroupCreateView(CreateView):
     model = GroupProductModel
-    form_class= GroupProductForm
+    form_class = GroupProductForm
     template_name = "group/CreateGroup.html"
+    success_url = reverse_lazy('inventary:ListGroup')
+
 
 class GroupUpdateView(UpdateView):
     model = GroupProductModel
-    form_class= GroupProductForm
+    form_class = GroupProductForm
     template_name = "group/UpdateGroup.html"
+
+    def get_success_url(self):
+        group_id = self.kwargs['pk']
+        return reverse_lazy('inventary:DetailGroup', kwargs={'pk': group_id})
+
 
 class GroupListView(ListView):
     model = GroupProductModel
-    context_object_name ="groups"
+    context_object_name = "groups"
     template_name = "group/ListGroup.html"
+
+
+
+class GroupDetailView(DetailView):
+    model = GroupProductModel
+    context_object_name="group"
+    template_name = "group/DetailGroup.html"
+
+
+class ShoppingCartListView(ListView):
+    model = ShoppingCartModel
+    context_object_name="products"
+    template_name = "shoppingcart/ListShopping.html"
+
+class ShoppingCartCreateView(CreateView):
+    model = ShoppingCartModel
+    template_name = "shoppingcart/CreateShopping.html"
+    success_url = reverse_lazy('inventary:ListShopping')
 
 
 
